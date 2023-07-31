e57e738c476d190dcd279139ab0c2f6a4e05bc1f
Luigi <posea.luigi99@gmail.com>
2023-07-31 09:19:06 +0300

    commit initial

---
+++ src/main/java/curs16/TestSelenium.java
@@ -0,0 +1,13 @@
+package curs16;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class TestSelenium {
+
+	@Test
+	public void testOpenBrowser() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://keybooks.ro/");
+	}
+}
